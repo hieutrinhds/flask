0a266175791c46acfd8bdf50f5840f4fbfd4f27a
hieutrinhds <hieutrinh.au@gmail.com>
2020-09-07 00:15:26 +0700

    modified app.py and templates

--- 03_Question_and_Answer/app.py
+++ 03_Question_and_Answer/app.py
@@ -1,6 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, g, request
+from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        db = get_db()
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
+        db.execute('insert into users (name, password, expert, admin) values(?, ?, ?, ?)', [request.form['name'], hashed_password, '0', '0'])
+        db.commit()
+        return '<h1>User created</h1>'
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return'<h1>Username: {} Password: {}</h1>'.format(request.form['name'], request.form['password'])
+    return render_template('login.html')
+
+@app.route('/question')
+def question():
+    return render_template('question.html')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
+
+@app.route('/ask')
+def ask():
+    return render_template('ask.html')
+
+@app.route('/unanswered')
+def unanswered():
+    return render_template('unanswered.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
 if __name__ == "__main__":
-    print("Hello")
+    app.run(debug=True)
